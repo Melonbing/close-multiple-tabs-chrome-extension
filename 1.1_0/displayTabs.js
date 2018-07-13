b2a4719fe8f54fbd982220ad4a3c893c404a4d9b
Tom Guo <tomguo1729@gmail.com>
2018-07-12 23:08:35 -0700

    delete displayTabs.js

--- 1.1_0/displayTabs.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    chrome.tabs.query({}, function (tabs) {
-	for (var i = 0; i < tabs.length; i++) {
-	var elem = document.createElement("h3");
-	elem.innerHTML = tabs[i].title;
-	document.body.appendChild(elem);
-    }	      
-});
-};
